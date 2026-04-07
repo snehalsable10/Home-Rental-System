980a4300660c0c9c044770b752f2c10cc783daa6
snehalsable10 <snehalsable105@hmail.com>
2026-04-07 19:02:03 +0530

    Update middleware configuration

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log(req.user);
     if(!req.isAuthenticated()) {
         req.flash("error", "you must be logged in to create listing!");
         return res.redirect("/login");
